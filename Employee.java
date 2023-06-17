e76f7d3ee55039e0dc0de7df24af83d18f8905ed
robinbenitezmora <robinbenitez1990@gmail.com>
2023-06-16 22:09:09 -0500

    Create methods in Employee

---
+++ Employee.java
@@ -0,0 +1,37 @@
+public class Employee {
+ private String name;
+ private String document;
+ private double salary;
+
+ public Employee() {
+
+ }
+
+ public String getName() {
+  return name;
+ }
+
+ public void setName(String name) {
+  this.name = name;
+ }
+
+ public String getDocument() {
+  return document;
+ }
+
+ public void setDocument(String document) {
+  this.document = document;
+ }
+
+ public double getSalary() {
+  return salary;
+ }
+
+ public void setSalary(double salary) {
+  this.salary = salary;
+ }
+
+ public double getBonus() {
+  return this.salary * 0.10;
+ }
+}
