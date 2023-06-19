d5eee451240cd546894b3152fa20a25da5f17ff3
robinbenitezmora <robinbenitez1990@gmail.com>
2023-06-19 17:13:18 -0500

    Modify 'TestReference' class

---
+++ TestReference.java
@@ -0,0 +1,20 @@
+public class TestReference {
+ public static void main(String[] args) {
+  Manager maria = new Manager();
+  maria.setName("Maria");
+  maria.setSalary(5000.0);
+
+  VideoEditor joao = new VideoEditor();
+  joao.setSalary(2500.0);
+
+  Designer jose = new Designer();
+  jose.setSalary(2000.0);
+
+  BonusControl bonusControl = new BonusControl();
+  bonusControl.register(maria);
+  bonusControl.register(joao);
+  bonusControl.register(jose);
+
+  System.out.println("Current calculus: " + bonusControl.getSum());
+ }
+}
