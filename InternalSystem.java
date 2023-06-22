2e6eba6db9dcc4a7700702a4062ac052583b9eb3
robinbenitezmora <robinbenitez1990@gmail.com>
2023-06-21 21:36:39 -0500

    Modify 'InternalSystem' class

--- InternalSystem.java
+++ InternalSystem.java
@@ -1,14 +1,13 @@
 public class InternalSystem {
  private String password = "2222";
 
- public boolean authenticate(Manager manager) {
-  boolean authenticated = manager.startSession(this.password);
+ public void authenticate(Authenticable manager) {
+  boolean authenticated = manager.authenticate(this.password);
   if (authenticated) {
    System.out.println("Welcome to the system!");
-   return true;
   } else {
    System.out.println("Access denied!");
-   return false;
   }
  }
+
 }
