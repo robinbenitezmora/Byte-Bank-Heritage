346d43fa97d755262081ae200a8697b4326850a7
robinbenitezmora <robinbenitez1990@gmail.com>
2023-06-22 22:01:27 -0500

    Refactor 'Manager' class

--- Manager.java
+++ Manager.java
@@ -1,31 +1,18 @@
 public class Manager implements Authenticable {
- private String keyword;
 
- public void setKeyword(String keyword) {
-  this.keyword = keyword;
- }
+ private UsefulAuthentication authenticator;
 
- public boolean startSession(String keyword) {
-  return keyword == "Alura";
- }
-
- public double getBonus() {
-  return this.getSalary() * 0.05;
- }
-
- private double getSalary() {
-  return 0;
+ public Manager() {
+  this.authenticator = new UsefulAuthentication();
  }
 
  @Override
- public void setPassword(String password) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'setPassword'");
+ public void setPassword(int password) {
+  this.authenticator.setPassword(password);
  }
 
  @Override
- public boolean authenticate(String password) {
-  // TODO Auto-generated method stub
-  throw new UnsupportedOperationException("Unimplemented method 'authenticate'");
+ public boolean authenticate(int password) {
+  return this.authenticator.authenticate(password);
  }
 }
